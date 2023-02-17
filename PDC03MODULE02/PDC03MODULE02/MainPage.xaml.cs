32824b05f011df91df267db52d028d31b178e30f
ten <fernandez.kirstenpearl@auf.edu.ph>
2023-02-17 13:33:28 +0800

    added activity 3 and 4

--- PDC03MODULE02/PDC03MODULE02/MainPage.xaml.cs
+++ PDC03MODULE02/PDC03MODULE02/MainPage.xaml.cs
@@ -24,5 +24,15 @@ namespace PDC03MODULE02
         {
             await Navigation.PushAsync(new activity2());
         }
+
+        private async void OpenActivity3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity3());
+        }
+
+        private async void OpenActivity4(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity4());
+        }
     }
 }
